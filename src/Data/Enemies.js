8c42024c5df19930c90e06c38670d70c76b23154
Jeronimo <jeronimotornini@gmail.com>
2023-04-27 23:18:58 -0300

    Creacion de Enemigos

---
+++ src/Data/Enemies.js
@@ -0,0 +1,37 @@
+export default Enemies = [
+  {
+    id: 1,
+    nombre: "Luke Skywalker",
+    alias: "Jedi Knight",
+    pelicula: "Star Wars: Episode IV - A New Hope",
+    salud: 100,
+  },
+  {
+    id: 2,
+    nombre: "Indiana Jones",
+    alias: "Archaeologist",
+    pelicula: "Raiders of the Lost Ark",
+    salud: 90,
+  },
+  {
+    id: 3,
+    nombre: "Harry Potter",
+    alias: "The Boy Who Lived",
+    pelicula: "Harry Potter and the Philosopher's Stone",
+    salud: 80,
+  },
+  {
+    id: 4,
+    nombre: "Tony Stark",
+    alias: "Iron Man",
+    pelicula: "Iron Man",
+    salud: 95,
+  },
+  {
+    id: 5,
+    nombre: "Hermione Granger",
+    alias: "The Brightest Witch of Her Age",
+    pelicula: "Harry Potter and the Chamber of Secrets",
+    salud: 85,
+  },
+];
